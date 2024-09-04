8ff96fefe8b96b3397a34d7a5de144dadc6b09be
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-04 15:05:03 +0200

    added new Class

--- 3_6_3_ts_a1_klassen_main.dart
+++ 3_6_3_ts_a1_klassen_main.dart
@@ -1,6 +1,7 @@
 // 3_6_3_ts_a1_klassen_main.dart
 import "dart:io";
 import "a1_class_helloworld.dart";
+import "a2_class_have_a_nice_day.dart";
 
 void main() {
   stdout.write("\x1B[2J\x1B[0;0H"); // entfernt die Zusatz-Info im Terminal
@@ -12,5 +13,9 @@ void main() {
   print(printhello.PrintOut());
   print("-------------------------------------------------------");
   print("Aufgabe 2:");
+  HaveANiceDay printhave = HaveANiceDay("Klaus", "");
+  print(printhave.PrintOut() + printhave.name);
+  HaveANiceDay setNewName = HaveANiceDay("Peter", "");
+  print("Der neue Name heißt ${setNewName.name}.");
   print("-------------------------------------------------------");
 }
