05ce46ef59b718d11126338079eab09f1bdca4f7
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-04 15:05:31 +0200

    coded new Class

---
+++ a2_class_have_a_nice_day.dart
@@ -0,0 +1,22 @@
+// a2_class_have_a_nice_day.dart
+/* Guten Tag, $Name
+Schreibe eine Klasse, die mit einem Namen instanziiert wird
+und eine Methode hat, die den Namen mit "Guten Tag, " davor
+auf der Konsole ausgibt.
+Schreibe eine Methode, die den Namen auf einen anderen Namen setzt.*/
+class HaveANiceDay {
+  String name;
+  String printOut;
+  HaveANiceDay(this.name,this.printOut);
+
+void newName(String newName) {
+    print("Der neue Name lautet: $newName");
+  }
+
+
+
+  String PrintOut() {
+    printOut = "Guten Tag, ";
+    return printOut;
+  }
+}
